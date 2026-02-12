edcc7a1f598ae2b64bc4048c98d1fd2c71a8f42a
ddduartediegoyahoo <dd.duartediego@yahoo.com>
2026-02-12 18:53:54 -0300

    Update layout.tsx

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { redirect } from "next/navigation";
 import { getProfile } from "@/lib/auth/get-profile";
 import { TeacherHeader } from "./_components/TeacherHeader";
