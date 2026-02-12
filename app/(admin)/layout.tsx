3b939b33948cd63df14e203362fd811b0f646d40
ddduartediegoyahoo <dd.duartediego@yahoo.com>
2026-02-12 18:53:33 -0300

    Update layout.tsx

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { redirect } from "next/navigation";
 import { getProfile } from "@/lib/auth/get-profile";
 import { AdminSidebar } from "./_components/AdminSidebar";
